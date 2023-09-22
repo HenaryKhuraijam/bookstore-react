c92f6f1942bca4ea85004e285e256487ff53ae44
HenaryKhuraijam <henarykhu123@gmial.com>
2023-09-22 20:58:10 +0530

    Set up Add book and book

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Book from './components/Book';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <HashRouter>
+    <Routes>
+      <Route index element={<Book />} />
+    </Routes>
+  </HashRouter>
+);
 
 export default App;
